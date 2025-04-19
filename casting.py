a536e2b634633e51885e4f26a372e1af19a486be
ViswaaE <viswaaeswaran@gmail.com>
2025-04-20 00:32:14 +0530

    casting and input from user topics covered

---
+++ casting.py
@@ -0,0 +1,40 @@
+#converting one datatype to another datatype
+
+'''a=int("10")
+b=int("20")
+c=a+b
+print(c)'''
+
+
+#getting input from user 
+'''x=int(input())
+y=int(input())
+z=x+y
+print(z)'''
+
+
+
+#get input for variable name and age and print it 
+#sample input EMC,1
+#sample Output "my name is :emc","My age is :1"
+
+"""m=input()
+n=int(input())
+o=input()
+print("My name is :",m)
+print("My age is :",n)
+print("My Address is :",o)"""
+
+#Get integer Input for variable a, b, c.
+#Multiply all 3 variables(a*b*c)
+#add all 3 variable (a+b+c) Divide the Multiplied value by Added Values Print it.
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=a*b*c
+e=a+b+c
+f=d/e
+print(d)
+print(e)
+print(f)
