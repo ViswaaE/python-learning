3f2bf45bd80b60c43d6985b6aa49f33ad43fa566
ViswaaE <viswaaeswaran@gmail.com>
2025-04-20 08:44:08 +0530

    forloop

---
+++ forloop.py
@@ -0,0 +1,22 @@
+#Use a for loop to print even numbers from 1 to 10.
+
+for i in range(1,11):
+    if i%2==0:
+        print(i)
+
+
+for i in range(2,11,2):
+    print(i)
+
+
+
+    #for variable in range(start, stop, step):
+    # code block
+for i in range(5):
+    print("Hello", i)
+
+for i in range(1, 6):   # 1 to 5
+    print(i)
+
+for i in range(10, 0, -1):  # countdown
+    print(i)
